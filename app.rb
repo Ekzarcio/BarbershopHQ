f59461dab157dd70c1cf0391519beb6b1c13c0a3
Ekzarcio <evg1093@gmail.com>
2020-10-15 21:06:30 +0300

    Add saving clients into db

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-set :database, {adapter: "postgresql", database: "Barbershop", port: "5432", user: "postgres", password: "P123#dfr"}
+set :database, {adapter: "postgresql", database: "Barbershop", user: "postgres"}
 
 class Client < ActiveRecord::Base
 end
@@ -11,6 +11,25 @@ end
 class Barber < ActiveRecord::Base
 end
 
+before do
+	@barbers = Barber.all
+end
+
 get '/' do
-	erb "Hello"
+	erb :index
+end
+
+get '/visit' do 
+	erb :visit
+end
+
+post '/visit' do
+	@name = params[:name]
+	@phone = params[:phone]
+	@datestamp = params[:datestamp]
+	@barber = params[:barber]
+
+	Client.create :name => @name, :phone => @phone, :datestamp => @datestamp, :barber => @barber 
+
+	erb "Ок, ждем Вас!"
 end
